4894ee8173ae48a211e2bd396c56015dae157582
ManuCode92 <74792972+ManuCode92@users.noreply.github.com>
2022-11-14 23:11:44 +0100

    Delete nttdatatallermvn-jmbj directory

--- nttdatatallermvn-jmbj/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.nttdata.mvn","l":"App"}];updateSearchResults();
--- nttdatatallermvn-jmbj/target/site/testapidocs/javadoc.bat
+++
@@ -1,1 +0,0 @@
-cmd.exe /X /C "C:\BootCamp\Desarollo\Tools\Java\jdk-18.0.2\bin\javadoc.exe @options @packages @argfile"
